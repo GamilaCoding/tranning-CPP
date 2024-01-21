cbdc0bec23e53c92b648f3ff5c36bb9a1c3a775c
Computer programmer <123306614+gameliaprogrammer@users.noreply.github.com>
2024-01-21 13:37:20 +0200

    Tranning cpp

---
+++ enums&if.cpp
@@ -0,0 +1,46 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+enum enScreenColor
+{
+  Red = 1,
+  Blue = 2,
+  Green = 3,
+  Yellow = 4
+};
+
+int main()
+{
+  cout << " *************\n";
+  cout << "Please choose number of your color \n";
+  cout << "(1) Red\n";
+  cout << "(2) Blue\n";
+  cout << "(3) Green\n";
+  cout << "(4) Yellow\n";
+  cout << "*************\n\n";
+  cout << "Your Choice? ";
+
+  int c;
+  enScreenColor Color;
+
+  cin >> c;
+  Color = (enScreenColor)c;
+  if (Color == enScreenColor::Red)
+  {
+    system("color 4F");
+  }
+  else if (Color == enScreenColor::Blue)
+  {
+    system("color 1F");
+  }
+  else if (Color == enScreenColor::Green)
+  {
+    system("color 2F");
+  }
+  else if (Color == enScreenColor::Yellow)
+  {
+    system("color 6F");
+  };
+  return 0;
+}
